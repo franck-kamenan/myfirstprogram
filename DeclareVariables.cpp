48fb4cf0874e26440052a3f39b1e8858bdcb5f3d
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-26 16:37:23 +0200

    Declare Multiple Variables

---
+++ DeclareVariables.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int monNr = 15;
+	cout << monNr << "\n";
+	int mon2emeNr;
+	mon2emeNr = 25;
+	cout << mon2emeNr << "\n";
+	int mon3emeNr = 35;
+	mon3emeNr = 45;
+	cout << mon3emeNr << "\n";
+	double myFloatNum = 6.66;
+	cout << myFloatNum << "\n";
+	char myLetter = 'D';
+	cout << myLetter << "\n";
+	string myText = "hello \n";
+	bool myBoolean = true;
+	cout << myBoolean << "\n";
+	int monAge = 35;
+	cout << "J'ai " << monAge << " ans. \n";
+	int a = 5;
+	int b = 6;
+	int sum = a + b;
+	cout << sum << "\n";
+	int x = 7, y = 8, z = 15;
+	cout << x + y + z << "\n";
+	return 0;
+}
