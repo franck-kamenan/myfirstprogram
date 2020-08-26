c1b1a51bbbf8365b309ac411396b4b3c541339d9
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-26 17:56:59 +0200

    DataTypes

---
+++ DataTypes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+//#include <string>
+using namespace std;
+
+int main()
+{
+	int monNr = 5;
+	float monFloatNr = 6.66;
+	double monDoubleNr = 9.98;
+	char maLettre = 'D';
+	bool monBoolean = true;
+	string monTexte = "Salut";
+	float f1 = 12e3;
+	double d1 = 33E4;
+	bool isCodingFun = true;
+	bool isFishTasty = false;
+	char myGrade = 'B';
+	string greeting = "Hello";
+
+	cout << monNr << "\n";
+	cout << monFloatNr << "\n";
+	cout << monDoubleNr << "\n";
+	cout << maLettre << "\n";
+	cout << monBoolean << "\n";
+	cout << monTexte << "\n";
+	cout << f1 << "\n";
+	cout << d1 << "\n";
+	cout << isCodingFun << "\n";
+	cout << isFishTasty << "\n";
+	cout << myGrade << "\n";
+	cout << greeting << "\n";
+
+	return 0;
+}
