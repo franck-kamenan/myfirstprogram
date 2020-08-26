14887932e7dc373476f5556e078384bc9ae68830
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-26 16:58:21 +0200

    Constants

---
+++ Constants.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int minutesPerHour = 60;
+	const float PI = 3.14;
+	
+	cout << minutesPerHour << "\n";
+	cout << PI << "\n";
+	return 0;;
+}
